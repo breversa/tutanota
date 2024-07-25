4e77332e63eb6edf38e8568ed52837af94f37079
mup <mup@tutao.de>
2024-07-25 16:21:10 +0200

    Fixes migrations and build file names

--- src/common/api/worker/offline/migrations/sys-v104.ts
+++ src/common/api/worker/offline/migrations/sys-v104.ts
@@ -1,7 +1,7 @@
 import { OfflineMigration } from "../OfflineStorageMigrator.js"
 import { OfflineStorage } from "../OfflineStorage.js"
 import { SqlCipherFacade } from "../../../../native/common/generatedipc/SqlCipherFacade.js"
-import { addValue, migrateAllListElements } from "../StandardMigrations.js"
+import { addValue, deleteInstancesOfType, migrateAllListElements } from "../StandardMigrations.js"
 import { PushIdentifierTypeRef } from "../../../entities/sys/TypeRefs.js"
 
 import { AppType } from "../../../common/TutanotaConstants.js"
@@ -10,6 +10,6 @@ export const sys104: OfflineMigration = {
 	app: "sys",
 	version: 104,
 	async migrate(storage: OfflineStorage, _: SqlCipherFacade) {
-		await migrateAllListElements(PushIdentifierTypeRef, storage, [addValue("app", AppType.Integrated)])
+		await deleteInstancesOfType(storage, PushIdentifierTypeRef)
 	},
 }
